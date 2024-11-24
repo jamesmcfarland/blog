87a4a03abbeb9cf08dd0d75e01426394ce6d945b
James McFarland <24841376+jamesmcfarland@users.noreply.github.com>
2024-11-24 01:32:52 +0000

    initial commit: add mdx, shad and test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
-
+import createMDX from "@next/mdx";
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  experimental: {
+    mdxRs: {
+      mdxType: "gfm",
+    },
+  },
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
---
+++ src/components/helloworld.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./ui/button";
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <Button>I&apos;m running in markdown!</Button>
+    </div>
+  );
+};
+
+export default HelloWorld;
